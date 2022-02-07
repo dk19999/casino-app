c9e94d1217d0281c12ad584cd3b344b35646c6a3
dk19999 <rattandeepak1999@gmail.com>
2022-02-07 10:14:20 +0530

    add avatar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,64 +2,94 @@ import { Button } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { useContext } from 'react';
 import { UserContext } from '../../App';
-import './index.css'
+import './index.css';
 
 const useStyles = makeStyles({
+  loginLogoutBtn: {
+    marginLeft: 20,
+    borderColor: '#117A8B',
+    textTransform: 'uppercase',
+    padding: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    cursor: 'pointer',
+  },
+  freeCreditBtn: {
+    padding: 10,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    cursor: 'pointer',
+    marginLeft: 30,
+  },
+});
 
-  loginLogoutBtn:{
-    marginLeft:20,
-    borderColor:'#117A8B',
-    textTransform:'uppercase',
-    padding:10,
-    backgroundColor:'white',
-    borderWidth:1,
-    cursor:'pointer'
-  }, 
-  freeCreditBtn:{
-    padding:10,
-    backgroundColor:'white',
-    borderWidth:1,
-    cursor:'pointer',
-    marginLeft:30
-  }
+export default function Header({ toggleLoginModal }) {
+  const { userData, setUserData } = useContext(UserContext);
 
-})
-
-export default function Header({toggleLoginModal}) {
-
-  const {userData, setUserData} = useContext(UserContext)
-
-  const classes = useStyles()
+  const classes = useStyles();
 
   const onClickLogout = () => {
-
-    setUserData(state => ({...state, name:'', loggedIn:false, spinData:[], balance:0}))
-
-  }
+    setUserData((state) => ({
+      ...state,
+      name: '',
+      loggedIn: false,
+      spinData: [],
+      balance: 0,
+    }));
+  };
 
   const giveFreeCredits = () => {
-    setUserData(state => ({...state, balance:state.balance+100}))
-  }
+    setUserData((state) => ({ ...state, balance: state.balance + 100 }));
+  };
 
   return (
-    <div className='Header'>
-      <div className='Header__main'>
+    <div className="Header">
+      <div className="Header__main">
         <div>
           <span>Deepak Kumar</span>
-          {
-            userData.balance < 100 && <button onClick={giveFreeCredits} className = {classes.freeCreditBtn}>Get 100$ for free</button>
-
-          }
+          {userData.balance < 100 && (
+            <button onClick={giveFreeCredits} className={classes.freeCreditBtn}>
+              Get 100$ for free
+            </button>
+          )}
         </div>
-        <div >
-          <span>Balance : {userData.balance}</span>
-          {
-            userData.loggedIn?(
-              <button className={classes.loginLogoutBtn} onClick={onClickLogout}>Logout</button>):(
-              <button className={classes.loginLogoutBtn} variant ={'outlined'} onClick={toggleLoginModal}>Login</button>)
-            }
+        <div className='flex align-center'>
+          <span>Balance : {userData.balance}$</span>
+          {userData.loggedIn ? (
+            <>
+              
+              <div className='flex align-center ' id = 'userInfo'>
+                <span  className='capitalize'>{userData.name}</span>
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 496 512"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"></path>
+                </svg>
+              </div>
+              <button
+                className={classes.loginLogoutBtn}
+                onClick={onClickLogout}
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <button
+              className={classes.loginLogoutBtn}
+              variant={'outlined'}
+              onClick={toggleLoginModal}
+            >
+              Login
+            </button>
+          )}
         </div>
-      </div>    
+      </div>
     </div>
   );
 }
